d95935dc6bd86ecfec23e61f49fdfc9464ef0cb3
ezeun <consistency1231@gmail.com>
2024-11-04 17:00:20 +0900

    Fix: elasticsearch korean analyzer 옵션 제거

--- backend/src/main/java/com/ssafy/fiftyninesec/search/entity/EventRoomSearch.java
+++ backend/src/main/java/com/ssafy/fiftyninesec/search/entity/EventRoomSearch.java
@@ -8,7 +8,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.time.LocalDateTime;
 
 @Data
-@Document(indexName = "eventrooms", createIndex = false)
+@Document(indexName = "eventrooms")
 public class EventRoomSearch {
 
     @Id
@@ -16,10 +16,10 @@ public class EventRoomSearch {
 
     private Integer memberId;
 
-    @Field(type = FieldType.Text, analyzer = "korean_analyzer")
+    @Field(type = FieldType.Text)
     private String title;
 
-    @Field(type = FieldType.Text, analyzer = "korean_analyzer")
+    @Field(type = FieldType.Text)
     private String description;
 
     @Field(type = FieldType.Keyword)
