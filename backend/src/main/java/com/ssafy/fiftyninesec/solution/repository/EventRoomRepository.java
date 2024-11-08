62e27d4f105f7a4b5c4e454d4a8d58f25d96ba2b
cmbear314 <cmbear314@naver.com>
2024-11-08 16:16:30 +0900

    Chore: eventRoom에 비관적락 사용

--- backend/src/main/java/com/ssafy/fiftyninesec/solution/repository/EventRoomRepository.java
+++ backend/src/main/java/com/ssafy/fiftyninesec/solution/repository/EventRoomRepository.java
@@ -3,10 +3,12 @@ package com.ssafy.fiftyninesec.solution.repository;
 import com.ssafy.fiftyninesec.solution.entity.EventRoom;
 
 import com.ssafy.fiftyninesec.solution.entity.Member;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -37,4 +39,8 @@ public interface EventRoomRepository extends JpaRepository<EventRoom, Long> {
     Optional<EventRoom> findLatestEventByMemberId(@Param("memberId") Long  memberId);
     List<EventRoom> findByMember(Member member, Sort sort);
 
+    // 비관적 잠금을 사용하는 메서드
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT e FROM EventRoom e WHERE e.id = :roomId")
+    Optional<EventRoom> findByIdForUpdate(@Param("roomId") Long roomId);
 }
