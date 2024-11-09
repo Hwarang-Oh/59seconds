80665047ccf8fce7b971a9ae1bd500b8cbf1c111
u1qns <u1qns@naver.com>
2024-11-09 16:43:34 +0900

    Feat: 검색어 연동 API

--- frontend/1s-before/src/components/common/SearchComponent.tsx
+++ frontend/1s-before/src/components/common/SearchComponent.tsx
@@ -1,6 +1,22 @@
+'use client';
+import { fetchSearchResults } from '@/apis/searchAPI';
 import SearchIcon from '@/components/icon/SearchIcon';
+import { useState } from 'react';
 
 export default function SearchComponent() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = async () => {
+      if(!searchTerm)
+          return;
+      try {
+        const results = await fetchSearchResults(searchTerm);
+        console.log('검색 결과:', results); // 결과 출력
+      } catch (error) {
+        console.error('검색 중 오류 발생:', error);
+      }
+  };
+
   return (
     <div className='flex items-center justify-between px-4 rounded-[25px] border-[3px] border-search-border w-[450px] h-[50px] cursor-pointer'>
       <input
@@ -8,8 +24,12 @@ export default function SearchComponent() {
         placeholder:text-search-input placeholder:font-normal placeholder:leading-[18px]
         focus:outline-none focus:border-transparent'
         placeholder='관심있는 이벤트를 검색해보세요.'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <SearchIcon />
+        <button onClick={handleSearch}>
+          <SearchIcon />
+        </button>
     </div>
   );
 }
