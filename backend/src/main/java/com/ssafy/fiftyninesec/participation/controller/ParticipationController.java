b99b3ce6383ce07c677c80c54a30e04c24f8c67f
ezeun <consistency1231@gmail.com>
2024-11-11 17:16:18 +0900

    Fix: ranking key 초기화 api 생성

--- backend/src/main/java/com/ssafy/fiftyninesec/participation/controller/ParticipationController.java
+++ backend/src/main/java/com/ssafy/fiftyninesec/participation/controller/ParticipationController.java
@@ -6,6 +6,7 @@ import com.ssafy.fiftyninesec.participation.entity.Participation;
 import com.ssafy.fiftyninesec.participation.service.ParticipationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,7 @@ import java.util.List;
 public class ParticipationController {
 
     private final ParticipationService participationService;
+    private final RedisTemplate<String, String> redisTemplate;
 
     // 참여자 목록 조회 API
     @GetMapping("/{roomId}/result")
@@ -29,5 +31,19 @@ public class ParticipationController {
     public ResponseEntity<ParticipationResponseDto> createParticipation(@Valid @RequestBody ParticipationRequestDto request) {
         ParticipationResponseDto responseDto = participationService.saveParticipation(request.getEventId(), request.getMemberId());
         return ResponseEntity.ok(responseDto);
+    }
+
+
+    private static final String RANKING_KEY_PREFIX = "event:ranking:";
+
+    // 특정 roomId에 대한 ranking 초기화
+    @PostMapping("/reset-ranking/{roomId}")
+    public ResponseEntity<String> resetRanking(@PathVariable Long roomId) {
+        String rankingKey = RANKING_KEY_PREFIX + roomId;
+
+        // Redis에서 해당 rankingKey 삭제
+        redisTemplate.delete(rankingKey);
+
+        return ResponseEntity.ok("Ranking for roomId " + roomId + " has been reset to start from 1.");
     }
 }
