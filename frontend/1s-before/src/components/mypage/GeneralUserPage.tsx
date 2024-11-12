1ecf8cc5eadfbffc90d0a41ee5c13c94ed5d9b96
Hwarang <keyein123@gmail.com>
2024-11-12 10:51:09 +0900

    🚑  buildTest

--- frontend/1s-before/src/components/mypage/GeneralUserPage.tsx
+++ frontend/1s-before/src/components/mypage/GeneralUserPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
 import GeneralUserEdit from './GeneralUserEdit';
 import Banner from '@/assets/defaultBanner.png';
 import { useEventRoom } from '@/hooks/eventRoomHook';
@@ -22,40 +23,29 @@ const GeneralUserPage = () => {
       <GeneralUserEdit />
 
       <section>
-        <div className="text-xl font-semibold border-b pb-4">
-          <span className="border-b-4 border-mainColor1 pb-4">
-            참여한 이벤트
-          </span>
+        <div className='text-xl font-semibold border-b pb-4'>
+          <span className='border-b-4 border-mainColor1 pb-4'>참여한 이벤트</span>
         </div>
-        <div className="flex space-x-3 mt-10">
+        <div className='flex space-x-3 mt-10'>
           <button
             onClick={() => setFilter('전체')}
             className={`px-4 py-1 rounded-full ${
-              filter === '전체'
-                ? 'bg-mainColor1 text-white'
-                : 'bg-gray-200 text-gray-700'
-            }`}
-          >
+              filter === '전체' ? 'bg-mainColor1 text-white' : 'bg-gray-200 text-gray-700'
+            }`}>
             전체
           </button>
           <button
             onClick={() => setFilter('당첨')}
             className={`px-4 py-1 rounded-full ${
-              filter === '당첨'
-                ? 'bg-mainColor1 text-white'
-                : 'bg-gray-200 text-gray-700'
-            }`}
-          >
+              filter === '당첨' ? 'bg-mainColor1 text-white' : 'bg-gray-200 text-gray-700'
+            }`}>
             당첨
           </button>
           <button
             onClick={() => setFilter('미당첨')}
             className={`px-4 py-1 rounded-full ${
-              filter === '미당첨'
-                ? 'bg-mainColor1 text-white'
-                : 'bg-gray-200 text-gray-700'
-            }`}
-          >
+              filter === '미당첨' ? 'bg-mainColor1 text-white' : 'bg-gray-200 text-gray-700'
+            }`}>
             미당첨
           </button>
         </div>
@@ -63,34 +53,26 @@ const GeneralUserPage = () => {
 
       {loading && <p>Loading...</p>}
 
-      <div className="mt-6 grid grid-cols-1 gap-6">
+      <div className='mt-6 grid grid-cols-1 gap-6'>
         {filteredRooms.length === 0 ? (
-          <p className="text-gray-500 text-center mt-10">참여한 이벤트 없음</p>
+          <p className='text-gray-500 text-center mt-10'>참여한 이벤트 없음</p>
         ) : (
           filteredRooms.map((room) => (
-            <div
-              key={room.eventId}
-              className="p-4 border border-gray-300 rounded-lg"
-            >
-              <img
-                src={room.bannerImage || Banner.src}
-                onError={(e) => {
-                  (e.target as HTMLImageElement).src = Banner.src;
-                }}
-                alt="배너"
-                className="w-full h-56 rounded-lg object-cover"
+            <div key={room.eventId} className='p-4 border border-gray-300 rounded-lg'>
+              <Image
+                src={room.bannerImage ?? Banner}
+                alt='배너'
+                className='w-full h-56 rounded-lg object-cover'
               />
-              <div className="flex flex-row justify-between items-center mx-1">
+              <div className='flex flex-row justify-between items-center mx-1'>
                 <div
                   className={`mt-3 flex items-center ${
                     room.isWinner ? 'text-subColor5' : 'text-subColor3'
-                  }`}
-                >
+                  }`}>
                   <span
                     className={`mr-3 rounded-2xl px-2 py-1 ${
                       room.isWinner ? 'bg-subColor4' : 'bg-gray-200'
-                    }`}
-                  >
+                    }`}>
                     {room.isWinner ? '당첨' : '미당첨'}
                   </span>
                   <span>
@@ -98,14 +80,12 @@ const GeneralUserPage = () => {
                   </span>
                 </div>
                 {room.isWinner && (
-                  <button className="mt-3 bg-mainColor1 text-white py-2 px-4 rounded-md">
+                  <button className='mt-3 bg-mainColor1 text-white py-2 px-4 rounded-md'>
                     정보 입력하고 상품 GET!
                   </button>
                 )}
               </div>
-              <p className="mt-2 ml-2 text-gray-600 font-bold text-lg">
-                {room.title}
-              </p>
+              <p className='mt-2 ml-2 text-gray-600 font-bold text-lg'>{room.title}</p>
             </div>
           ))
         )}
