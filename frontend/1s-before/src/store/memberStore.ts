14486b810ee36f29472013950ff75361a0092ba5
Hwarang <keyein123@gmail.com>
2024-11-13 11:45:19 +0900

    ✨ feat : Image Test and socket Test

--- frontend/1s-before/src/store/memberStore.ts
+++ frontend/1s-before/src/store/memberStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, devtools } from 'zustand/middleware';
 
 interface MemberState {
   memberId: number;
@@ -9,37 +9,46 @@ interface MemberState {
 
 interface MemberStore {
   member: MemberState | null;
-  setMember: (memberId: number, nickname: string) => void;
+  setMember: (memberId: number, nickname: string, isCreatorMode: boolean) => void;
   clearMember: () => void;
   toggleCreatorMode: () => void;
 }
 
+// 클라이언트 환경에서 sessionStorage를 사용하여 초기 상태 설정
 export const useMemberStore = create<MemberStore>()(
-  persist(
-    (set) => ({
-      member: null,
-      setMember: (memberId, nickname) =>
-        set({
-          member: { memberId, nickname, isCreatorMode: false },
-        }),
-      clearMember: () => set({ member: null }),
-      toggleCreatorMode: () =>
-        set((state) => ({
-          member: state.member
-            ? { ...state.member, isCreatorMode: !state.member.isCreatorMode }
+  devtools(
+    persist(
+      (set) => ({
+        member:
+          typeof window !== 'undefined' && sessionStorage.getItem('member-storage')
+            ? JSON.parse(sessionStorage.getItem('member-storage') as string)
             : null,
-        })),
-    }),
-    {
-      name: 'member-storage',
-      storage: {
-        getItem: (name) => {
-          const storedValue = sessionStorage.getItem(name);
-          return storedValue ? JSON.parse(storedValue) : null;
-        },
-        setItem: (name, value) => sessionStorage.setItem(name, JSON.stringify(value)),
-        removeItem: (name) => sessionStorage.removeItem(name),
-      },
-    }
+        setMember: (memberId, nickname, isCreatorMode = false) =>
+          set({
+            member: { memberId, nickname, isCreatorMode },
+          }),
+        clearMember: () => set({ member: null }),
+        toggleCreatorMode: () =>
+          set((state) => ({
+            member: state.member
+              ? { ...state.member, isCreatorMode: !state.member.isCreatorMode }
+              : null,
+          })),
+      }),
+      {
+        name: 'member-storage',
+        storage:
+          typeof window !== 'undefined'
+            ? {
+                getItem: (name) => {
+                  const storedValue = sessionStorage.getItem(name);
+                  return storedValue ? JSON.parse(storedValue) : null;
+                },
+                setItem: (name, value) => sessionStorage.setItem(name, JSON.stringify(value)),
+                removeItem: (name) => sessionStorage.removeItem(name),
+              }
+            : undefined,
+      }
+    )
   )
 );
