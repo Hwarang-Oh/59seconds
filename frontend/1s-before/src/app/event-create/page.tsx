0c909822e25fd9b4e69107c4b9aa0150f52ba349
Hwarang <keyein123@gmail.com>
2024-11-13 17:50:44 +0900

    ✨ feat : 마지막 불꽃 2

--- frontend/1s-before/src/app/event-create/page.tsx
+++ frontend/1s-before/src/app/event-create/page.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/common/Header';
 import EventOwnerCreate from '@/components/eventCreate/EventOwnerCreate';
 import PreviewComponent from '@/components/eventCreate/PreviewComponent';
 import EventDetailCreate from '@/components/eventCreate/EventDetailCreate';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEventCreate } from '@/hooks/eventCreateHook';
 import { useEventOwner } from '@/hooks/eventOwnerHook';
 
@@ -28,39 +28,33 @@ export default function EventCreate() {
   return (
     <>
       <Header />
-      <div className="m-10">
-        <h1 className="text-mainColor1 text-3xl font-bold text-center mb-10">
-          이벤트 생성하기
-        </h1>
-        <div className="grid grid-cols-4 gap-8">
+      <div className='m-10'>
+        <h1 className='text-mainColor1 text-3xl font-bold text-center mb-10'>이벤트 생성하기</h1>
+        <div className='grid grid-cols-4 gap-8'>
           {/* 입력 폼: 2/3 */}
-          <div className="col-span-3 border border-inherit p-10 rounded-lg shadow-lg">
-            <form className="p-6 max-w-screen-xl mx-auto space-y-4">
+          <div className='col-span-3 border border-inherit p-10 rounded-lg shadow-lg'>
+            <form className='p-6 max-w-screen-xl mx-auto space-y-4'>
               <EventDetailCreate />
             </form>
-            <form className="p-6 mx-auto max-w-screen-xl space-y-6">
+            <form className='p-6 mx-auto max-w-screen-xl space-y-6'>
               <EventOwnerCreate />
             </form>
 
-            <div className="flex justify-between">
-              <button
-                type="button"
-                className="py-2 px-4 bg-gray-300 rounded text-gray-700"
-              >
+            <div className='flex justify-between'>
+              <button type='button' className='py-2 px-4 bg-gray-300 rounded text-gray-700'>
                 이전
               </button>
               <button
-                type="button"
+                type='button'
                 onClick={handleCreateEvent}
-                className="py-2 px-4 bg-mainColor1 text-white rounded"
-              >
+                className='py-2 px-4 bg-mainColor1 text-white rounded'>
                 이벤트 생성하기
               </button>
             </div>
           </div>
 
           {/* 미리보기: 1/3 */}
-          <div className="col-span-1 max-h-[70vh] border border-inherit p-10 rounded-lg shadow-lg">
+          <div className='col-span-1 max-h-[70vh] border border-inherit p-10 rounded-lg shadow-lg'>
             <PreviewComponent />
           </div>
         </div>
