c2f2a754a28a9d961c7d685c297a14b2c0ee1325
ezeun <consistency1231@gmail.com>
2024-11-14 14:23:09 +0900

    메인클래스 이름 변경

--- backend/src/main/java/com/ssafy/fiftyninesec/ParticipationApplication.java
+++ backend/src/main/java/com/ssafy/fiftyninesec/ParticipationApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class FiftyninesecApplication {
+public class ParticipationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FiftyninesecApplication.class, args);
+		SpringApplication.run(ParticipationApplication.class, args);
 	}
 
 }
