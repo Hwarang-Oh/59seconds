28d8ab390c8d369c8767628ecec4c0a8633cd567
u1qns <u1qns@naver.com>
2024-11-16 03:22:51 +0900

    Initialize: Clean out the Repository - 2

--- backend/src/main/java/com/ssafy/fiftyninesec/search/dto/EventRoomSearchRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.fiftyninesec.search.dto;
-
-import lombok.Data;
-
-@Data
-public class EventRoomSearchRequestDto {
-    private String keyword; // 검색에 사용할 키워드
-    private Long memberId;  // 회원 ID
-}
--- backend/src/main/java/com/ssafy/fiftyninesec/solution/entity/EventStatus.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.fiftyninesec.solution.entity;
-
-public enum EventStatus {
-    NOT_STARTED,           // 이벤트 시작 전
-    ONGOING,               // 이벤트 진행 중
-    COMPLETED,             // 이벤트가 정상적으로 종료됨
-    COMPLETED_NO_WINNER_INFO // 당첨자는 다 채워졌으나 당첨 정보 입력 미완료
-}
