1677823f86f6c392de31e526b8aacd638e83ff51
Hwarang <keyein123@gmail.com>
2024-11-15 17:41:21 +0900

    🔥 hotfix : for buiild

--- frontend/1s-before/src/hooks/useMemberLoginHook.ts
+++ frontend/1s-before/src/hooks/useMemberLoginHook.ts
@@ -1,33 +1,31 @@
-import { useSearchParams, useRouter } from 'next/navigation';
+'use client';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { fetchTokens } from '@/apis/memberAPI';
 import { useMemberStore } from '@/store/memberStore';
 
 export const useMemberLogin = () => {
-  const searchParams = useSearchParams();
+  const [isClient, setIsClient] = useState(false); // 클라이언트 환경 확인
+  const searchParams = isClient ? useSearchParams() : null;
   const router = useRouter();
-  const code = searchParams.get('code');
+  const code = searchParams?.get('code');
   const { member, setMember, clearMember, toggleCreatorMode } = useMemberStore();
   const [isLoginPopUpOpen, setIsLoginPopUpOpen] = useState(false);
+
   const openLoginPopUp = () => setIsLoginPopUpOpen(true);
   const closeLoginPopUp = () => setIsLoginPopUpOpen(false);
+
   const handleKakaoLogin = () => {
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&response_type=code`;
     window.location.href = KAKAO_AUTH_URL;
   };
 
-  useEffect(() => {
-    if (code) {
-      console.log(code);
-      handleKakaoLoginCallBack(code);
-    }
-  }, [code]);
-
   const handleKakaoLoginCallBack = async (authCode: string) => {
     try {
       const tokenData = await fetchTokens(authCode);
       console.log(tokenData);
-      router.replace(window.location.pathname);
+      router.replace(window.location.pathname); // URL에서 `code` 제거
     } catch (error) {
       console.error('Error while fetching tokens:', error);
     }
@@ -38,9 +36,20 @@ export const useMemberLogin = () => {
   };
 
   const handleToggle = () => {
-    if (member.isLoggedIn) toggleCreatorMode(); // Member가 존재할 때만 CreatorMode를 토글합니다.
+    if (member.isLoggedIn) toggleCreatorMode();
     else openLoginPopUp();
   };
+
+  useEffect(() => {
+    setIsClient(true); // 클라이언트 환경 설정
+  }, []);
+
+  useEffect(() => {
+    if (code) {
+      console.log(`Auth Code: ${code}`);
+      handleKakaoLoginCallBack(code);
+    }
+  }, [code]);
 
   return {
     member,
