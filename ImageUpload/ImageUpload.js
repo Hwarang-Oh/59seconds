592c54ccc71873d7e8f61201059eb4fcea23805a
Hwarang <keyein123@gmail.com>
2024-11-17 21:25:54 +0900

    ✨ feat : chat Message 확인 및 Image 경로 설정 완료!

--- ImageUpload/ImageUpload.js
+++ ImageUpload/ImageUpload.js
@@ -49,22 +49,45 @@ createFolders()
   .catch((error) => console.error('Error creating folders:', error));
 
 /** IMP : SQL 쿼리문
-Select * from event_room;
+// Banner, Rectangle, Square 이미지 경로 업데이트
 UPDATE dreamsolution.event_room
 SET 
     banner_image = CASE
-        WHEN (room_id) % 3 = 1 THEN CONCAT('https://k11a404.p.ssafy.io/', room_id, '/Banner_Do.jpg')
-        WHEN (room_id) % 3 = 2 THEN CONCAT('https://k11a404.p.ssafy.io/', room_id, '/Banner_Yena.jpg')
-        WHEN (room_id) % 3 = 0 THEN CONCAT('https://k11a404.p.ssafy.io/', room_id, '/Banner_Eunbin.jpg')
+        WHEN (room_id) % 3 = 1 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Banner_Do.jpg')
+        WHEN (room_id) % 3 = 2 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Banner_Yena.jpg')
+        WHEN (room_id) % 3 = 0 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Banner_Eunbin.jpg')
         ELSE banner_image -- 기존 값 유지
     END,
     rectangle_image = CASE
-        WHEN (room_id) % 3 = 1 THEN CONCAT('https://k11a404.p.ssafy.io/', room_id, '/Rectangle_Do.jpg')
-        WHEN (room_id) % 3 = 2 THEN CONCAT('https://k11a404.p.ssafy.io/', room_id, '/Rectangle_Yena.jpg')
-        WHEN (room_id) % 3 = 0 THEN CONCAT('https://k11a404.p.ssafy.io/', room_id, '/Rectangle_Eunbin.jpg')
+        WHEN (room_id) % 3 = 1 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Rectangle_Do.jpg')
+        WHEN (room_id) % 3 = 2 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Rectangle_Yena.jpg')
+        WHEN (room_id) % 3 = 0 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Rectangle_Eunbin.jpg')
         ELSE rectangle_image -- 기존 값 유지
+    END,
+    square_image = CASE 
+        WHEN (room_id) % 3 = 1 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Rectangle_Do.jpg')
+        WHEN (room_id) % 3 = 2 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Rectangle_Yena.jpg')
+        WHEN (room_id) % 3 = 0 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/', room_id, '/Rectangle_Eunbin.jpg')
+        ELSE square_image -- 기존 값 유지
     END
 WHERE room_id > 0;
 
+// 프로필 이미지 경로 업데이트
+
+UPDATE dreamsolution.member
+SET 
+    profile_image = CASE
+        WHEN (member_id) % 9 = 1 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member1.jpg')
+        WHEN (member_id) % 9 = 2 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member2.jpg')
+        WHEN (member_id) % 9 = 3 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member3.jpg')
+        WHEN (member_id) % 9 = 4 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member4.jpg')
+        WHEN (member_id) % 9 = 5 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member5.jpg')
+        WHEN (member_id) % 9 = 6 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member6.jpg')
+        WHEN (member_id) % 9 = 7 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member7.jpg')
+        WHEN (member_id) % 9 = 8 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member8.jpg')
+        WHEN (member_id) % 9 = 0 THEN CONCAT('https://k11a404.p.ssafy.io/event-image/member/member9.jpg')
+        ELSE profile_image -- 기존 값 유지
+    END
+WHERE member_id > 0;
 commit;
 */
