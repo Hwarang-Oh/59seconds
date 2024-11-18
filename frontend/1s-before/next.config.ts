8abe62145750b8f8f51fd562a26e4687bcf000ff
Hwarang <keyein123@gmail.com>
2024-11-19 02:12:48 +0900

    ✅ ✨ feat : 임시 로그인 주석 처리, 및 Production Error 무시 처리 ✅ common -> HeaderInfo 에서 수정함 ✅ next.config.js에서 수정함

--- frontend/1s-before/next.config.ts
+++ frontend/1s-before/next.config.ts
@@ -10,6 +10,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // TODO : NextJS는 기본적으로 Error Message를 배포 환경에서는 숨겨주긴 한다.
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
