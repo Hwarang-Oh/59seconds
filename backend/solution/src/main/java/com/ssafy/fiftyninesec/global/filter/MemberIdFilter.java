f3b72b2a5032b51ea316f6f07f77cb0840f0a9f4
ParkSangwoo <psw1253@naver.com>
2024-11-19 09:49:07 +0900

    feat: 필터 수정

--- backend/solution/src/main/java/com/ssafy/fiftyninesec/global/filter/MemberIdFilter.java
+++ backend/solution/src/main/java/com/ssafy/fiftyninesec/global/filter/MemberIdFilter.java
@@ -1,18 +1,26 @@
 package com.ssafy.fiftyninesec.global.filter;
 
+import com.ssafy.fiftyninesec.global.util.JwtUtil;
 import jakarta.servlet.*;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class MemberIdFilter implements Filter {
+     private final JwtUtil jwtUtil;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
@@ -23,6 +31,20 @@ public class MemberIdFilter implements Filter {
             httpRequest.setAttribute("memberId", Long.parseLong(memberId));
         }
 
+        Optional<Cookie> accessTokenCookie = Arrays.stream(Optional.ofNullable(httpRequest.getCookies()).orElse(new Cookie[0]))
+                 .filter(cookie -> "accessToken".equals(cookie.getName()))
+                 .findFirst();
+
+        if (accessTokenCookie.isPresent()) {
+         String accessToken = accessTokenCookie.get().getValue();
+         //액세스 토큰 검증
+         if (jwtUtil.validateToken(accessToken)) {
+             Long temp = jwtUtil.getMemberIdFromToken(accessToken);
+             httpRequest.setAttribute("memberId", temp);
+             chain.doFilter(request, response);
+             return; // 성공 시 필터 체인 실행 후 종료
+              }
+        }
         chain.doFilter(httpRequest, httpResponse);
     }
 
